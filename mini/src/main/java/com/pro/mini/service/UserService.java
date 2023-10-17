b0a12f6edaf737f2ce571cf018fcff549e4bba43
CHOCO <jh780465@naver.com>
2023-10-17 22:31:01 +0900

    [Service] UserService created

---
+++ mini/src/main/java/com/pro/mini/service/UserService.java
@@ -0,0 +1,40 @@
+package com.pro.mini.service;
+
+import com.pro.mini.dao.UserDAO;
+import com.pro.mini.repository.UserRepository;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserDAO saveUser(UserDAO userDAO){
+        return userRepository.save(userDAO);
+    }
+
+    public List<UserDAO> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<UserDAO> getUserById(Integer id){
+        return userRepository.findById(id);
+    }
+
+    public List<UserDAO> getUsersByEmail(String email){
+        return userRepository.findByEmail(email);
+    }
+
+    public List<UserDAO> getUsersByName(String name){
+        return userRepository.findByName(name);
+    }
+
+    public void deleteUser(Integer id){
+        userRepository.deleteById(id);
+    }
+}
