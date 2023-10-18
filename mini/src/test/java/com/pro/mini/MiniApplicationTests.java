9df3d9c03e29200eddbd867aac5e5820d35c57df
CHOCO <jh780465@naver.com>
2023-10-18 09:52:15 +0900

    [JUnit5] Failure - Tried to use JUnit5 for the first time ... : Going to Test in MiniApplication.java → Not enough time for getting knowledgeable JUnit5 ■■■■■■■■■■■■■■■■■■■■■■■■■■■ Error Log ■■■■■■■■■■■■■■■■■■■■■■■■■■ java.lang.IllegalStateException: Failed to load ApplicationContext for [WebMergedContextConfiguration@2041c9a3 testClass = com.pro.mini.MiniApplicationTests, locations = [], classes = [com.pro.mini.MiniApplication] ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■

--- mini/src/test/java/com/pro/mini/MiniApplicationTests.java
+++ mini/src/test/java/com/pro/mini/MiniApplicationTests.java
@@ -1,13 +1,62 @@
 package com.pro.mini;
 
-import org.junit.jupiter.api.Test;
+import com.pro.mini.dao.GroupDAO;
+import com.pro.mini.dao.InvitationDAO;
+import com.pro.mini.dao.MemberDAO;
+import com.pro.mini.dao.UserDAO;
+import com.pro.mini.service.GroupService;
+import com.pro.mini.service.InvitationService;
+import com.pro.mini.service.MemberService;
+import com.pro.mini.service.UserService;
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MiniApplicationTests {
 
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
+	private UserService userService;
+	private GroupService groupService;
+	private MemberService memberService;
+	private InvitationService invitationService;
+
+	private UserDAO userDAO;
+	private GroupDAO groupDAO;
+	private MemberDAO memberDAO;
+	private InvitationDAO invitationDAO;
+	@BeforeAll
+	void setUp() {
+		userDAO = new UserDAO("test_user", "010-0000-0000", "test@gmail.com");
+		groupDAO = new GroupDAO("test_group");
+		memberDAO = new MemberDAO(groupDAO, userDAO, "Y");
+		invitationDAO = new InvitationDAO("/invites/link?test", "N", "N", groupDAO, userDAO);
+
+		log.info("[DAOs] ::: " +
+				"[userDAO] == " + userDAO +
+				"[groupDAO] == " + groupDAO +
+				"[memberDAO] == " + memberDAO +
+				"[invitationDAO] == " + invitationDAO);
+
+
+		groupService.saveGroup(groupDAO);
+		memberService.saveMember(memberDAO);
+		invitationService.saveInvitation(invitationDAO);
+
+	}
 	@Test
-	void contextLoads() {
+	@Order(1)
+	void saveUser() {
+		setUp();
+		userService.saveUser(userDAO);
+		List<UserDAO> userDAOList = userService.getAllUsers();
+		log.info("userDAOList === {}", userDAOList);
 	}
 
 }
