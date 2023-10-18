8947d59ec645c43eb25f01fd5c47680c65452db8
CHOCO <jh780465@naver.com>
2023-10-18 11:36:48 +0900

    [Controller] UserController.java created.

---
+++ mini/src/main/java/com/pro/mini/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.pro.mini.controller;
+
+import com.pro.mini.vo.UserVO;
+import com.pro.mini.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserVO>> getAllUsers() {
+        List<UserVO> userVOList = userService.getAllUsers();
+        log.info("userDAOList ::: {}", userVOList);
+        return new ResponseEntity<List<UserVO>>(userVOList, HttpStatus.OK);
+    }
+}
